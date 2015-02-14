6dde4ad5758cb5189655533396021d7821c3fea5
Dominik Surowiecki <dominik223@gmail.com>
2015-02-14 20:34:17 +0100

    Added test

--- src/test/scala/HelloSpec.scala
+++ src/test/scala/HelloSpec.scala
@@ -2,7 +2,17 @@ import org.scalacheck.{Gen, Arbitrary, Properties}
 import org.scalacheck.Prop.forAll
 
 object StringSpecification extends Properties("String") {
-  //val evenInteger = Gen.containerOf[List,Int](Gen.oneOf(1, 3, 5)) suchThat(_.foldLeft(_ != _))
-  //property("startsWith") = forAll {
+
+  property("startsWith") = forAll { (a: String, b: String) =>
+    (a+b).startsWith(a)
+  }
+
+  property("concatenate") = forAll { (a: String, b: String) =>
+    (a+b).length > a.length && (a+b).length > b.length
+  }
+
+  property("substring") = forAll { (a: String, b: String, c: String) =>
+    (a+b+c).substring(a.length, a.length+b.length) == b
+  }
 
 }
